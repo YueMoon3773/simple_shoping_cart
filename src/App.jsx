2edafdadf6a66917d93a9fc61efee6aa65f48718
YueMoon <60178169+YueMoon3773@users.noreply.github.com>
2025-10-25 16:22:55 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.scss';
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1>Hi</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+import routes from './routes/routes';
+import App from './App.jsx';
+
+import './index.scss';
+
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App />
+        {/* <RouterProvider router={router} /> */}
+    </StrictMode>,
+);
